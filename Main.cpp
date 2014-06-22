9bc51513746e8c872ae4da7546626fc5c3d06f18
Aman13 <sandhu.amandeep.s@gmail.com>
2014-06-22 16:08:08 -0700

    Working merge and insertion sort

--- Main.cpp
+++ Main.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <stdlib.h>
 
-int merge(int arr[], int temp[], int low, int mid, int high)	{
+int merge(int arr[], int temp[], int low, int mid, int high, int counter)	{
 	int lower = low;
 	int index = low;
 	int pivot = mid+1;
@@ -19,38 +19,39 @@ int merge(int arr[], int temp[], int low, int mid, int high)	{
 			pivot++;
 		}
 		index++;
+		counter++;
 	}
 	if(lower > mid)	{
 		for(int i = pivot; i <= high; ++i)	{
 			temp[index] = arr[i];
 			index++;
+			counter++;
 		}
 	}
 	else	{
 		for(int i = lower; i <= mid; ++i)	{
 			temp[index] = arr[i];
 			index++;
+			counter++;
 		}
 	}
 
 	for(int i = low; i <= high; ++i)	{
-		std::cout << "final array builder" << std::endl;
 		arr[i] = temp[i];
 	}
-	return 0;
+	return counter;
 
 }
 
-int merge_builder(int arr[], int temp[], int low, int high)	{
-	std::cout << "merge builder" << std::endl;
+int merge_builder(int arr[], int temp[], int low, int high, int counter)	{
 	int mid;
 	if(low < high)	{
 		mid = (low+high)/2;
-		merge_builder(arr, temp, low, mid);
-		merge_builder(arr, temp, mid+1, high);
-		merge(arr, temp, low, mid, high);
+		counter = merge_builder(arr, temp, low, mid, counter);
+		counter = merge_builder(arr, temp, mid+1, high, counter);
+		counter = merge(arr, temp, low, mid, high, counter);
 	}
-	return 0;
+	return counter;
 }
 
 int number_swap(int arr[], int i, int counter)	{
@@ -86,106 +87,32 @@ int merge_sort(int arr[], int n)	{
 	<< "-------------" << std::endl << std::endl;
 
 	int temp[n];
-
-	merge_builder(arr, temp, 0, n-1);
+	int barometer = 0;
+	barometer = merge_builder(arr, temp, 0, n-1, barometer);
 
 	for(int i = 0; i < n; ++i)	{
 		std::cout << arr[i] << std::endl;
 	}
-	return 13;
+	return barometer;
 }
 
 int main()	{
-	int n = rand() % 10 + 5;
+	int n = rand() % 200 + 50;
 	int arr[n];
 	for(int i = 0; i < n; ++i)	{
 		arr[i] = rand() % 10;
-		std::cout << arr[i] << std::endl;
+		std::cout << arr[i] << " ,";
 	}
 
-	//int a = insertion_sort(arr, n);
-	//std::cout << "Insertion sort barometer: " << a << std::endl;
+	int a = insertion_sort(arr, n);
+	std::cout << "Insertion sort barometer: " << a << std::endl;
 
-	int b = merge_sort(arr, n);
-	std::cout << "Merge sort barometer: " << b << std::endl;
+	for(int i = 0; i < n; ++i)	{
+		arr[i] = rand() % 10;
+		std::cout << arr[i] << " , ";
+	}
 
-	int num = sizeof(arr)/sizeof(int);
-	std::cout << "sizeof(arr) =" << sizeof(arr) << std::endl
-	<< std::endl << "size = " << num << std::endl
-	<< "n = " << n << std::endl;
+	int d = merge_sort(arr, n);
+	std::cout << "Merge sort barometer: " << d << std::endl;
+
 }
-
-/*
- 
-void merge(int *a, int *b, int low, int pivot, int high)
-{
-    int h,i,j,k;
-    h=low;
-    i=low;
-    j=pivot+1;
- 
-    while((h<=pivot)&&(j<=high))
-    {
-        if(a[h]<=a[j])
-        {
-            b[i]=a[h];
-            h++;
-        }
-        else
-        {
-            b[i]=a[j];
-            j++;
-        }
-        i++;
-    }
-    if(h>pivot)
-    {
-        for(k=j; k<=high; k++)
-        {
-            b[i]=a[k];
-            i++;
-        }
-    }
-    else
-    {
-        for(k=h; k<=pivot; k++)
-        {
-            b[i]=a[k];
-            i++;
-        }
-    }
-    for(k=low; k<=high; k++) a[k]=b[k];
-}
-
-
-
-
-void mergesort(int *a, int *b, int low, int high)
-{
-    int pivot;
-    if(low<high)
-    {
-        pivot=(low+high)/2;
-        mergesort(a,b,low,pivot);
-        mergesort(a,b,pivot+1,high);
-        merge(a,b,low,pivot,high);
-    }
-}
-
-int main()
-{
-    int a[] = {12,10,43,23,-78,45,123,56,98,41,90,24};
-    int num;
- 
-    num = sizeof(a)/sizeof(int);
- 
-    int b[num];
- 
-    mergesort(a,b,0,num-1);
- 
-    for(int i=0; i<num; i++)	{
-        std::cout<<a[i]<<" ";
-    	std::cout<<std::endl;
-    }
-}
-*/
