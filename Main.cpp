7e0e932467793a23567443654218d98a732094cc
Aman13 <sandhu.amandeep.s@gmail.com>
2014-06-22 18:18:25 -0700

    Working on quick sort

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,34 @@
 #include <fstream>
 #include <stdlib.h>
 
+int number_swap(int arr[], int i, int counter)	{
+	int j = i;
+	int temp;
+	while(j > 0 && arr[j] < arr[j-1])	{
+		temp = arr[j];
+		arr[j] = arr[j-1];
+		arr[j-1] = temp;
+		++counter;
+		j--;
+	}
+	return counter;
+}
+
+int insertionsort(int arr[], int n)	{
+	std::cout << "Insertion Sort" << std::endl 
+	<< "-------------" << std::endl << std::endl;
+
+	int barometer = 0;
+	for(int i = 1; i < n; ++i)	{
+		barometer = number_swap(arr, i, barometer); 
+	}
+	for(int i = 0; i < n; ++i)	{
+		std::cout << arr[i] << std::endl;
+	}
+	
+	return barometer;
+}
+
 int merge(int arr[], int temp[], int low, int mid, int high, int counter)	{
 	int lower = low;
 	int index = low;
@@ -54,35 +82,7 @@ int merge_builder(int arr[], int temp[], int low, int high, int counter)	{
 	return counter;
 }
 
-int number_swap(int arr[], int i, int counter)	{
-	int j = i;
-	int temp;
-	while(j > 0 && arr[j] < arr[j-1])	{
-		temp = arr[j];
-		arr[j] = arr[j-1];
-		arr[j-1] = temp;
-		++counter;
-		j--;
-	}
-	return counter;
-}
-
-int insertion_sort(int arr[], int n)	{
-	std::cout << "Insertion Sort" << std::endl 
-	<< "-------------" << std::endl << std::endl;
-
-	int barometer = 0;
-	for(int i = 1; i < n; ++i)	{
-		barometer = number_swap(arr, i, barometer); 
-	}
-	for(int i = 0; i < n; ++i)	{
-		std::cout << arr[i] << std::endl;
-	}
-	
-	return barometer;
-}
-
-int merge_sort(int arr[], int n)	{
+int mergesort(int arr[], int n)	{
 	std::cout << "Merge sort" << std::endl 
 	<< "-------------" << std::endl << std::endl;
 
@@ -96,23 +96,92 @@ int merge_sort(int arr[], int n)	{
 	return barometer;
 }
 
-int main()	{
-	int n = rand() % 200 + 50;
-	int arr[n];
-	for(int i = 0; i < n; ++i)	{
-		arr[i] = rand() % 10;
-		std::cout << arr[i] << " ,";
+int quick_sorter(int arr[], int low, int high, int counter)	{
+	std::cout << "Entering quick_sorter" << std::endl;
+	int left = low;
+	int right = high-1;
+	int pivot = high;
+	int temp;
+
+	while(left < right)	{
+		while(arr[left] > arr[pivot] && right > left)	{
+			if(right < pivot)	{
+				temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+			}
+			right--;
+			counter++;
+		}
+		left++;
+		counter++;
+	}
+	while(left < pivot)	{
+		if(arr[left] > arr[pivot])	{
+			temp = arr[pivot];
+			arr[pivot] = arr[left];
+			arr[left] = temp;
+		}
+		left++;
+	}
+	for(int i = low; i <= high; ++i)	{
+		std::cout << arr[i] << std::endl;
 	}
 
-	int a = insertion_sort(arr, n);
+	return counter;
+}
+
+int quick_partioner(int arr[], int low, int high, int counter)	{
+	int mid;
+	if(low < high)	{
+		counter = quick_sorter(arr, low, high, counter);
+		int mid = (low+high)/2;
+		counter = quick_partioner(arr, low, mid, counter);
+		counter = quick_partioner(arr, mid+1, high, counter);
+	}
+
+	return counter;
+}
+
+int quicksort(int arr[], int n)	{
+	std::cout << "Quick Sort" << std::endl
+	<< "-------------" << std::endl << std::endl;
+
+	int barometer = 0;
+
+	barometer = quick_partioner(arr, 0, n-1, barometer);
+
+	std::cout << "SORTED ARRAY IS" << std::endl <<
+	"-----------" << std::endl;
+	for(int i = 0; i < n; ++i)	{
+		std::cout << arr[i] << std::endl;
+	}
+
+	return barometer;
+
+}
+
+
+int main()	{
+	int n = rand() % 10 + 10;
+	int arr[n];
+	for(int i = 0; i < n; ++i)	{
+		arr[i] = rand() % 100;
+		std::cout << arr[i] << ",";
+	}
+/*
+	int a = insertionsort(arr, n);
 	std::cout << "Insertion sort barometer: " << a << std::endl;
 
 	for(int i = 0; i < n; ++i)	{
-		arr[i] = rand() % 10;
-		std::cout << arr[i] << " , ";
+		arr[i] = rand() % 100;
+		std::cout << arr[i] << ", ";
 	}
 
-	int d = merge_sort(arr, n);
-	std::cout << "Merge sort barometer: " << d << std::endl;
+	int b = mergesort(arr, n);
+	std::cout << "Merge sort barometer: " << b << std::endl;
+*/
+	int c = quicksort(arr,n);
+	std::cout << "Quick sort barometer: " << c << std::endl;
 
 }
